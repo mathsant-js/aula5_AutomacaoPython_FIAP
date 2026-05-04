52c3bba4fba92ea4358b2733fdefbe0e4e25c6c3
mathsant-js <matheus.santana114@etec.sp.gov.br>
2026-05-04 11:18:00 -0300

    feat: trabalhando com matrizes e for each

---
+++ matrizes.py
@@ -0,0 +1,11 @@
+musicas = [
+    ["Baby", "Justin Bieber"],
+    ["Evidências", "Chitãozinho e Xororó"],
+    ["Boate Azul", "Bruno e Marrone"]
+]
+
+# for each
+for musica in musicas:
+    for info in musica:
+        print(info)
+    print()
