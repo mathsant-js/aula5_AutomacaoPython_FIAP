0fb01713968375e14017d178058af64eaa877d85
mathsant-js <matheus.santana114@etec.sp.gov.br>
2026-05-11 08:58:33 -0300

    feat: exercício 7 melhorado

--- exercicios/exercicio7.py
+++ exercicios/exercicio7.py
@@ -6,8 +6,20 @@ matriz = [
 
 for i in range(len(matriz)):
     for j in range(len(matriz[i])):
-        matriz[i][j] = input("Digite algo: ")
+        matriz[i][j] = input(f"Digite o valor para matriz[{i}][{j}]: ")
 
+# Loop Tradicional
+print("\n====Loop Tradicional====")
+for i in range(len(matriz)):
+    print("[", end=" ")
+    for j in range(len(matriz[i])):
+        print(matriz[i][j], end=", ")
+    print("]", end="")
+    print()
+
+# For-each
+print("\n====For-each====")
 for m in matriz:
     for n in m:
-        print(n)
+        print(n, end=" ")
+    print()
